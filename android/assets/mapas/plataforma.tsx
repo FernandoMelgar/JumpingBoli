5a223ff8cee5ffaf655efbc922df4fe3f1967f99
ClaudioMayoral <claudio_mayoral@hotmail.com>
2020-10-23 23:36:07 -0500

    agregue plataforma.tsx

---
+++ android/assets/mapas/plataforma.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="plataforma" tilewidth="32" tileheight="32" tilecount="70" columns="7">
+ <image source="Plataformas.png" width="224" height="320"/>
+ <tile id="44">
+  <properties>
+   <property name="plataformaD" value="tipo"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="plataforma" value="tipo"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="plataformaI" value="tipo"/>
+  </properties>
+ </tile>
+</tileset>
