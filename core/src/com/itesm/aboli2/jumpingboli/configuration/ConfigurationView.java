85c1426acd8e123ce0b2148ae4efc6a25e220597
artur3456 <a01376086@itesm.mx>
2020-10-28 20:28:16 -0600

    Implementación pantalla configuración.

--- core/src/com/itesm/aboli2/jumpingboli/configuration/ConfigurationView.java
+++ core/src/com/itesm/aboli2/jumpingboli/configuration/ConfigurationView.java
@@ -1,6 +1,7 @@
 package com.itesm.aboli2.jumpingboli.configuration;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -11,7 +12,9 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.itesm.aboli2.jumpingboli.GdXGame;
 import com.itesm.aboli2.jumpingboli.Pantalla;
+import com.itesm.aboli2.jumpingboli.button.GameButton;
 import com.itesm.aboli2.jumpingboli.menu.MenuView;
+import com.itesm.aboli2.jumpingboli.skins.Seleccion;
 
 public class ConfigurationView extends Pantalla {
 
@@ -28,13 +31,36 @@ public class ConfigurationView extends Pantalla {
   @Override
   public void show() {
     configurationStage = new Stage(super.viewport);
-    texturaFondo = new Texture("fondos/fondoExtra.png");
+    texturaFondo = new Texture("fondos/fondoAjustes.png");
     createConfigView();
+    cargarPreferencias();
     Gdx.input.setInputProcessor(configurationStage);
+  }
+
+  private void cargarPreferencias() {
+    cargarPreferenciasMusica();
+  }
+
+  private void cargarPreferenciasMusica() {
+    Preferences prefs = Gdx.app.getPreferences("Musica");
   }
 
   private void createConfigView() {
     createBackBtn();
+    crearBtnControlMusica();
+  }
+
+  private void crearBtnControlMusica() {
+    //Botón encendido.
+    ImageButton btnOn = new GameButton("buttons/btnOn.png", "buttons/btnOff.png");
+    btnOn.setPosition(ANCHO_PANTALLA * 0.61f, ALTO_PANTALLA * 0.58f, Align.center);
+    btnOn.addListener(new ClickListener() {
+      public void clicked(InputEvent event, float x, float y){
+        super.clicked(event, x, y);
+      }
+    });
+
+    configurationStage.addActor(btnOn);
   }
 
   private void createBackBtn() {
