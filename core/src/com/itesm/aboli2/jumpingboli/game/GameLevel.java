31b61ba9c50da79409767656c68a87c93b4ec3e9
Fernando Melgar <fernandom.melgar@gmail.com>
2020-11-03 23:25:30 -0600

    - Se agregan los métodos correspondientes a mostrar los escudos y la puntuación a la clase padre

--- core/src/com/itesm/aboli2/jumpingboli/game/GameLevel.java
+++ core/src/com/itesm/aboli2/jumpingboli/game/GameLevel.java
@@ -1,10 +1,13 @@
 package com.itesm.aboli2.jumpingboli.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -12,6 +15,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.itesm.aboli2.jumpingboli.GameText;
 import com.itesm.aboli2.jumpingboli.GdXGame;
 import com.itesm.aboli2.jumpingboli.Pantalla;
@@ -50,6 +55,11 @@ public abstract class GameLevel extends Pantalla {
   protected final float segundosReanudación = 3;
 
 
+  protected Stage stageHUD;
+  protected OrthographicCamera cameraHUD;
+  protected Viewport viewHUD;
+
+
   public GameLevel(GdXGame game) {
     super(game);
   }
@@ -65,15 +75,37 @@ public abstract class GameLevel extends Pantalla {
   public abstract void show_();
 
   protected void show_onlyOnStartUp() {
-
     levelStage = new Stage(super.viewport);
     assetManager = new AssetManager();
     onStartUp_initBackground();
     onStartUp_initGameText();
     onStartUp_initAudio();
     onStartUp_initMaps();
+    initHUD();
+    initShields();
     show_onStartOnly_();
 
+  }
+
+  protected void initShields() {
+    shieldTexture = new Texture("characters/escudo.png");
+    shieldArray = new Array<>();
+    for (int i = 1; i <= 3; i++)
+      shieldArray.add(new Escudo(shieldTexture, i * 30, ALTO_PANTALLA * 0.88f));
+  }
+
+  private void initHUD() {
+    cameraHUD = new OrthographicCamera(ANCHO_PANTALLA, ALTO_PANTALLA);
+    cameraHUD.position.set(ANCHO_PANTALLA / 2, ALTO_PANTALLA / 2, 0);
+    cameraHUD.update();
+    viewHUD = new StretchViewport(ANCHO_PANTALLA, ALTO_PANTALLA, cameraHUD);
+    stageHUD = new Stage(viewHUD);
+
+    ImageButton btnGPause = getPauseBtn();
+    ImageButton bntSalto = getJumpBtn();
+
+    stageHUD.addActor(bntSalto);
+    stageHUD.addActor(btnGPause);
   }
 
 
@@ -101,6 +133,8 @@ public abstract class GameLevel extends Pantalla {
   }
 
   protected void drawShields() {
+    if (shieldArray == null)
+      return;
     for (Escudo escudo : shieldArray)
       escudo.render(batch);
   }
@@ -161,7 +195,111 @@ public abstract class GameLevel extends Pantalla {
     return bntSalto;
   }
 
-  protected abstract void render_checkForCollisions();
+  protected void updateShields() {
+    if (shieldArray == null)
+      return;
+    for (int i = shieldArray.size - 1; i >= 0; i--) {
+      if (boli.getY() < 0) {
+        shieldArray.removeIndex(i);
+        break;
+      }
+    }
+  }
+
+  protected void updateScore() {
+    if (boli.getEstadoBuff() == EstadoBuff.BUFFDOBLEPUNTOS) {
+
+      timerBuffMultiplicador++;
+      score += 0.2f;
+
+      if (timerBuffMultiplicador / 60 > segundosBuff) {
+        timerBuffMultiplicador = 0;
+        boli.setEstadoBuff(EstadoBuff.NORMAL);
+      }
+    } else {
+      score += 0.1f;
+    }
+
+  }
+
+  protected boolean esBuffMultiplicador(TiledMapTileLayer.Cell celda) {
+    if (celda == null)
+      return false;
+    Object propiedad = celda.getTile().getProperties().get("tipo");
+    return "BuffMultiplicador".equals(propiedad);
+  }
+
+  protected boolean boliIsAlive() {
+    return (boli.getY() + boli.sprite.getHeight() > 0);
+  }
+
+  protected void drawResumeCountDown() {
+    batch.begin();
+    gameText.mostrarMensaje(batch, "" + (int) (3 - timerReanudacion / 60),
+        camera.position.x, camera.position.y);
+    timerReanudacion++;
+
+    batch.end();
+  }
+
+  protected boolean gameIsResuming() {
+    return gameStatus == GameStatus.REANUDANDO;
+  }
+
+  protected void DrawGetReadyCountDown() {
+    batch.begin();
+    gameText.mostrarMensaje(batch, "" + (int) (3 - timerPausa),
+        ANCHO_PANTALLA / 2, ALTO_PANTALLA / 2);
+    batch.end();
+  }
+
+  protected boolean gameIsStarting() {
+    return gameStatus == GameStatus.INICIANDO && timerPausa <= 3;
+  }
+
+  protected void updateGameStatusIfTimerRunsOut(float delta) {
+    timerPausa += delta;
+    if (gameStatus == GameStatus.INICIANDO && timerPausa >= 3) {
+      gameStatus = GameStatus.JUGANDO;
+    }
+  }
+
+  protected void startGameAgainIfTimerReanudacionRunsOut() {
+    if (timerReanudacion / 60 > segundosReanudación) {
+      gameStatus = GameStatus.JUGANDO;
+      backgroudMusic.play();
+      boli.setEstadoBoli(EstadoBoli.RODANDO);
+    }
+  }
+
+
+  protected void render_checkForCollisions() {
+    int celdaX = (int) (boli.getX() / TAM_CELDA);
+    int celdaY = (int) ((boli.getY() + 1.5 * boli.DY) / TAM_CELDA);
+
+
+    TiledMapTileLayer capa = (TiledMapTileLayer) map.getLayers().get(0);
+    TiledMapTileLayer.Cell celdaAbajo = capa.getCell(celdaX, celdaY);
+    TiledMapTileLayer.Cell celdaDerecha = capa.getCell(celdaX + 1, celdaY);
+
+
+    if (esBuffMultiplicador(capa.getCell(celdaX + 1, celdaY + 1))) {
+      Gdx.app.log("MUERTO", "F");      // Borrar esta estrella y contabilizar
+      capa.setCell(celdaX + 1, celdaY + 1, null);
+      boli.setEstadoBuff(EstadoBuff.BUFFDOBLEPUNTOS);
+    }
+    if ((celdaAbajo == null && celdaDerecha == null && boli.getEstado() != EstadoBoli.SALTANDO) || (esBuffMultiplicador(celdaAbajo) && boli.getEstado() != EstadoBoli.SALTANDO) || (esBuffMultiplicador(celdaDerecha) && boli.getEstado() != EstadoBoli.SALTANDO)) {
+      if (boli.getEstado() != EstadoBoli.CAYENDO) {
+        boli.setyBase(boli.getY());
+        boli.cayendo();
+      }
+    } else if (boli.getEstado() != EstadoBoli.SALTANDO) {
+      // Dejarlo sobre la celda que lo detiene
+      boli.setPosicion(boli.getX(), (celdaY + 1) * TAM_CELDA);
+      boli.setEstadoBoli(EstadoBoli.RODANDO);
+    }
+
+  }
 
   protected abstract void render_(float delta);
 
--- core/src/com/itesm/aboli2/jumpingboli/game/LevelOne.java
+++ core/src/com/itesm/aboli2/jumpingboli/game/LevelOne.java
@@ -3,26 +3,14 @@ package com.itesm.aboli2.jumpingboli.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 import com.itesm.aboli2.jumpingboli.GameText;
 import com.itesm.aboli2.jumpingboli.GdXGame;
 
 public class LevelOne extends GameLevel {
-
-
-  private Stage stageHUD;
-  private OrthographicCamera cameraHUD;
-  private Viewport viewHUD;
 
 
   public LevelOne(GdXGame game) {
@@ -32,36 +20,11 @@ public class LevelOne extends GameLevel {
   @Override
   protected void show_onStartOnly_() {
     boli = new Boli(new Texture("characters/boli_morado.png"), 200, 600);
-    initHUD();
-    initShields();
   }
 
   @Override
   protected void onStartUp_initGameText() {
     gameText = new GameText("fuentes/exoFont.fnt");
-  }
-
-  private void initHUD() {
-
-    cameraHUD = new OrthographicCamera(ANCHO_PANTALLA, ALTO_PANTALLA);
-    cameraHUD.position.set(ANCHO_PANTALLA / 2, ALTO_PANTALLA / 2, 0);
-    cameraHUD.update();
-    viewHUD = new StretchViewport(ANCHO_PANTALLA, ALTO_PANTALLA, cameraHUD);
-    stageHUD = new Stage(viewHUD);
-
-    ImageButton btnGPause = getPauseBtn();
-    ImageButton bntSalto = getJumpBtn();
-
-    stageHUD.addActor(bntSalto);
-    stageHUD.addActor(btnGPause);
-  }
-
-
-  private void initShields() {
-    shieldTexture = new Texture("characters/escudo.png");
-    shieldArray = new Array<>();
-    for (int i = 1; i <= 3; i++)
-      shieldArray.add(new Escudo(shieldTexture, i * 30, ALTO_PANTALLA * 0.88f));
   }
 
 
@@ -78,7 +41,6 @@ public class LevelOne extends GameLevel {
     assetManager.finishLoading();
     map = assetManager.get("mapas/platNivel1.tmx");
     mapRenderer = new OrthogonalTiledMapRenderer(map);
-
   }
 
   @Override
@@ -96,42 +58,6 @@ public class LevelOne extends GameLevel {
     backgroundTexture = new Texture("mapas/NivelUno.png");
   }
 
-
-  @Override
-  protected void render_checkForCollisions() {
-
-    int celdaX = (int) (boli.getX() / TAM_CELDA);
-    int celdaY = (int) ((boli.getY() + 1.5 * boli.DY) / TAM_CELDA);
-
-
-    TiledMapTileLayer capa = (TiledMapTileLayer) map.getLayers().get(0);
-    TiledMapTileLayer.Cell celdaAbajo = capa.getCell(celdaX, celdaY);
-    TiledMapTileLayer.Cell celdaDerecha = capa.getCell(celdaX + 1, celdaY);
-
-
-    if (esBuffMultiplicador(capa.getCell(celdaX + 1, celdaY + 1))) {
-      Gdx.app.log("MUERTO", "F");      // Borrar esta estrella y contabilizar
-      capa.setCell(celdaX + 1, celdaY + 1, null);
-      boli.setEstadoBuff(EstadoBuff.BUFFDOBLEPUNTOS);
-    }
-    if ((celdaAbajo == null && celdaDerecha == null && boli.getEstado() != EstadoBoli.SALTANDO) || (esBuffMultiplicador(celdaAbajo) && boli.getEstado() != EstadoBoli.SALTANDO) || (esBuffMultiplicador(celdaDerecha) && boli.getEstado() != EstadoBoli.SALTANDO)) {
-      if (boli.getEstado() != EstadoBoli.CAYENDO) {
-        boli.setyBase(boli.getY());
-        boli.cayendo();
-      }
-    } else if (boli.getEstado() != EstadoBoli.SALTANDO) {
-      // Dejarlo sobre la celda que lo detiene
-      boli.setPosicion(boli.getX(), (celdaY + 1) * TAM_CELDA);
-      boli.setEstadoBoli(EstadoBoli.RODANDO);
-    }
-  }
-
-  private boolean esBuffMultiplicador(TiledMapTileLayer.Cell celda) {
-    if (celda == null)
-      return false;
-    Object propiedad = celda.getTile().getProperties().get("tipo");
-    return "BuffMultiplicador".equals(propiedad);
-  }
 
   @Override
   protected void render_(float delta) {
@@ -161,18 +87,14 @@ public class LevelOne extends GameLevel {
     if (gameIsStarting())
       DrawGetReadyCountDown();
     else if (gameIsResuming()) {
-
       boli.sprite.setX(boli.getX());
       boli.sprite.setY(boli.getY());
 
       boli.setDX(0);
       boli.sprite.rotate(30);
-
       drawResumeCountDown();
       startGameAgainIfTimerReanudacionRunsOut();
-
     }
-
 
     if (boli.getEstado() == EstadoBoli.CAYENDO) {
 
@@ -204,77 +126,5 @@ public class LevelOne extends GameLevel {
     drawScore();
     batch.end();
     stageHUD.draw();
-
-
   }
-
-  private void drawResumeCountDown() {
-    batch.begin();
-    gameText.mostrarMensaje(batch, "" + (int) (3 - timerReanudacion / 60),
-        camera.position.x, camera.position.y);
-    timerReanudacion++;
-
-    batch.end();
-  }
-
-  private boolean gameIsResuming() {
-    return gameStatus == GameStatus.REANUDANDO;
-  }
-
-  private void DrawGetReadyCountDown() {
-    batch.begin();
-    gameText.mostrarMensaje(batch, "" + (int) (3 - timerPausa),
-        ANCHO_PANTALLA / 2, ALTO_PANTALLA / 2);
-    batch.end();
-  }
-
-  private boolean gameIsStarting() {
-    return gameStatus == GameStatus.INICIANDO && timerPausa <= 3;
-  }
-
-
-  private boolean boliIsAlive() {
-    return (boli.getY() + boli.sprite.getHeight() > 0);
-  }
-
-  private void updateGameStatusIfTimerRunsOut(float delta) {
-    timerPausa += delta;
-    if (gameStatus == GameStatus.INICIANDO && timerPausa >= 3) {
-      gameStatus = GameStatus.JUGANDO;
-    }
-  }
-
-  private void startGameAgainIfTimerReanudacionRunsOut() {
-    if (timerReanudacion / 60 > segundosReanudación) {
-      gameStatus = GameStatus.JUGANDO;
-      backgroudMusic.play();
-      boli.setEstadoBoli(EstadoBoli.RODANDO);
-    }
-  }
-
-  private void updateScore() {
-    if (boli.getEstadoBuff() == EstadoBuff.BUFFDOBLEPUNTOS) {
-
-      timerBuffMultiplicador++;
-      score += 0.2f;
-
-      if (timerBuffMultiplicador / 60 > segundosBuff) {
-        timerBuffMultiplicador = 0;
-        boli.setEstadoBuff(EstadoBuff.NORMAL);
-      }
-    } else {
-      score += 0.1f;
-    }
-
-  }
-
-  private void updateShields() {
-    for (int i = shieldArray.size - 1; i >= 0; i--) {
-      if (boli.getY() < 0) {
-        shieldArray.removeIndex(i);
-        break;
-      }
-    }
-  }
-
 }
