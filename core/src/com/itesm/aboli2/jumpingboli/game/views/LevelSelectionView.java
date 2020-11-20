52d92aa1fc429fc635efa787463edf31c22bd9e1
Fernando Melgar <fernandom.melgar@gmail.com>
2020-11-20 16:35:18 -0600

    Se crea vista de selección de niveles, falta implementar

---
+++ core/src/com/itesm/aboli2/jumpingboli/game/views/LevelSelectionView.java
@@ -0,0 +1,25 @@
+package com.itesm.aboli2.jumpingboli.game.views;
+
+import com.itesm.aboli2.jumpingboli.GdXGame;
+import com.itesm.aboli2.jumpingboli.Pantalla;
+
+public class LevelSelectionView extends Pantalla {
+  public LevelSelectionView(GdXGame context) {
+    super(context);
+  }
+
+  @Override
+  public void show() {
+
+  }
+
+  @Override
+  public void render(float delta) {
+
+  }
+
+  @Override
+  public void dispose() {
+
+  }
+}
